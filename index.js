6e3e84506004ff5c8e5d33b2e43e3da0f7c048fb
manu <manulol400@gmail.com>
2024-07-12 16:42:59 -0300

    Lista de monedas con APIs
    Jugando con las APIs de monedas para mostrarlo en una lista

--- index.js
+++ index.js
@@ -8,26 +8,32 @@ const apiDolar = "https://v6.exchangerate-api.com/v6/ca1f64be6b00014dc4bf44a1/la
 fetch(apiDolar)
  .then(response => response.json())
  .then(response => {
-
+     
+        Object.keys(response.conversion_rates).forEach(moneda => {
+         $("#lista").append($(`<li> ${moneda}: ${response.conversion_rates[moneda]} </li> `))
+            });
         const valorARSUSD = response.conversion_rates.ARS;
-        console.log(valorARSUSD);      
+        console.log(valorARSUSD);
+        console.log(response);      
+        console.log(response.conversion_rates);
         
         function calcularTotal(valorARSUSD){ 
+            const dolares = document.querySelector("#monto").value;
+            const resultado = document.querySelector("#resultado");
+            const resultadoTotal = valorARSUSD * dolares;
+            resultado.textContent = `el resultado es ARS$${resultadoTotal}`;
 
-        const dolares = document.querySelector("#monto").value;
-        const resultado = document.querySelector("#resultado");
-        const resultadoTotal = valorARSUSD * dolares;
-        resultado.textContent = `el resultado es ARS$${resultadoTotal}`;
+            console.log(resultadoTotal);
 
-        console.log(resultadoTotal);
-
-        return false;
+            return false;
         }        
-    const form = document.querySelector("#formulario");
-    form.addEventListener("submit", event => {
-        event.preventDefault();
-        calcularTotal(valorARSUSD);       
-
+        const form = document.querySelector("#formulario");
+        form.addEventListener("submit", event => {
+            event.preventDefault();
+            calcularTotal(valorARSUSD);
+        
+       
+    })
+    
  })
-
- })
+ .catch(error => console.error("FALLO",error));
